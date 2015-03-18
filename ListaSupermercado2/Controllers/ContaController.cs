bf36f8ab8334431af48aff5a2eed486d1ccb9cae
Eduardo <eduardo.s.tanaka@gmail.com>
2015-03-18 20:29:45 -0300

    v1.1

--- ListaSupermercado2/Controllers/ContaController.cs
+++ ListaSupermercado2/Controllers/ContaController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ListaSupermercado2.Models;
+using ListaSupermercado2.UnitsOfWork;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,15 +10,31 @@ namespace ListaSupermercado2.Controllers
 {
     public class ContaController : Controller
     {
+        private UnitOfWork _unit = new UnitOfWork();
+
         // GET: Conta
         public ActionResult Index()
         {
             return View();
         }
 
+        [HttpGet]
         public ActionResult Cadastrar()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Cadastrar(Conta conta)
+        {
+            _unit.ContaRepository.Add(conta);
+            _unit.Save();
+            return View();
+        }
+
+        public ActionResult Listar()
+        {
+            return View(_unit.ContaRepository.List());
         }
     }
 }
---
+++ ListaSupermercado2/Controllers/ContaWSController.cs
@@ -0,0 +1,66 @@
+﻿using ListaSupermercado2.Models;
+using ListaSupermercado2.UnitsOfWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace ListaSupermercado2.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class ContaWSController : ApiController
+    {
+        private UnitOfWork _unit = new UnitOfWork();
+
+        public IEnumerable<Conta> Get()
+        {
+            return _unit.ContaRepository.List();
+        }
+
+        public Conta Get(int id)
+        {
+            return _unit.ContaRepository.SearchById(id);
+        }
+
+        public IHttpActionResult Post(Conta conta)
+        {
+
+            if (ModelState.IsValid)
+            {
+                _unit.ContaRepository.Add(conta);
+                _unit.Save();
+                var uri = Url.Link("DefaultApi", new { id = conta.ContaId });
+                return Created<Conta>(new Uri(uri), conta);
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
+        }
+
+        public IHttpActionResult Put(int id, Conta conta)
+        {
+            if (ModelState.IsValid)
+            {
+                conta.ContaId = id;
+                _unit.ContaRepository.Update(conta);
+                _unit.Save();
+                return Ok(conta);
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
+        }
+
+        public void Delete(int id)
+        {
+            _unit.ContaRepository.Delete(id);
+            _unit.Save();
+        }
+
+    }
+}
