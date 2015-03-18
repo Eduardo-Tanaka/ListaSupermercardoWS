79b56d4eabe9fe83bf680c34cd88f2cc7e5d9cc0
Eduardo <eduardo.s.tanaka@gmail.com>
2015-03-18 20:16:50 -0300

    initial

---
+++ ListaSupermercado2/Controllers/ContaController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ListaSupermercado2.Controllers
+{
+    public class ContaController : Controller
+    {
+        // GET: Conta
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Cadastrar()
+        {
+            return View();
+        }
+    }
+}
---
+++ ListaSupermercado2/Models/Conta.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ListaSupermercado2.Models
+{
+    public class Conta
+    {
+        public int ContaId { get; set; }
+        public string Usuario { get; set; }
+        public string Senha { get; set; }
+    }
+}
