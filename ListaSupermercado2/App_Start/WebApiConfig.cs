e81308f6036fec91798bd6c1dd2324bff949c0a3
Eduardo <eduardo.s.tanaka@gmail.com>
2015-03-21 00:22:51 -0300

    12

--- ListaSupermercado2/App_Start/WebApiConfig.cs
+++ ListaSupermercado2/App_Start/WebApiConfig.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
@@ -23,6 +24,10 @@ namespace ListaSupermercado2
             config.EnableCors();
 
             config.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+            config.Formatters.JsonFormatter.Indent = true;
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
         }
     }
 }
