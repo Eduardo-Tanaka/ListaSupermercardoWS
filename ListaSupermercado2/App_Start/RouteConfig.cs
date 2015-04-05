c5834363f2cc86bed327ad0f7d5b2b1b2e23a34d
Eduardo <eduardo.s.tanaka@gmail.com>
2015-04-05 09:12:44 -0300

    v05/04..3

--- ListaSupermercado2/App_Start/RouteConfig.cs
+++ ListaSupermercado2/App_Start/RouteConfig.cs
@@ -15,7 +15,7 @@ namespace ListaSupermercado2
 
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}/{nome}",
+                url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Conta", action = "Logar", id = UrlParameter.Optional }
             );
         }
